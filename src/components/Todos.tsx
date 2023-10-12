338a8c384d20421986f1c6813a39f7e6a24f4680
Sam Schoenberg <sam4130@Sams-MacBook-Pro.local>
2023-10-12 17:00:00 -0600

    inital push

---
+++ src/components/Todos.tsx
@@ -0,0 +1,12 @@
+
+const ToDos: React.FC<{items: string[]}> = (props) =>{
+    return (
+        <ul>
+            {props.items.map(item =>(
+                <li key={item}>{item}</li>
+            ))}
+        </ul>
+    )
+};
+
+export default ToDos;
